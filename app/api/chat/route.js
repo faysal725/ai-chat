d686faa437e686238f7035ec431ee7281abc90eb
faysal725 <afaysal725@gmail.com>
2025-01-22 16:18:55 +0600

    shadcn added

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,19 +1,37 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from "openai";
 
-const config = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
-const openai = new OpenAIApi(config);
+const client = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { message } = req.body;
-    try {
-      const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: message }],
-      });
-      res.status(200).json({ reply: response.data.choices[0].message.content });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
+export async function POST(request) {
+  const { message } = await request.json();
+  try {
+    const params = {
+        messages: [{ role: 'developer', content: 'Say this is a test' }],
+        model: 'gpt-4o-mini-2024-07-18',
+      };
+      const chatCompletion = await client.chat.completions.create(params);
+
+       console.log(client.chat.completions.create(params))
+
+
+    //   res.status(200).json({ reply: response.data.choices[0].message.content });
+
+    //   return new Response(JSON.stringify(data), {
+    //     headers: { "Content-Type": "application/json" },
+    //     status: 201,
+    //   });
+
+    let data = { reply: "response.data.choices[0].message.content" };
+
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
+    });
+  } catch (error) {
+    console.log(error)
+    return  Response.json("error happened");
   }
 }
+
