68115957d920ebc18826b210366d06c36188b76d
faysal725 <afaysal725@gmail.com>
2025-01-22 16:31:00 +0600

    shadcn button

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main className="text-4xl">
-      jkghjk
+            <Button>Click me</Button>
+
     </main>
   );
 }
