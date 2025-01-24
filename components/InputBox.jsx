d6990579692e4215e22bade26a22102b3dcbdbd1
faysal725 <afaysal725@gmail.com>
2025-01-24 19:12:22 +0600

    query function added

--- components/InputBox.jsx
+++ components/InputBox.jsx
@@ -6,10 +6,34 @@ import { Button } from "./ui/button";
 export default function InputBox() {
   const queryTextRef = useRef(null);
 
-  function submit(event) {
+  async function submit(event) {
     event.preventDefault();
 
-    console.log(queryTextRef.current.value);
+    console.log(process.env["OPENAI_API_KEY"]);
+    await sendingQuery(queryTextRef.current.value)
+  }
+
+  async function sendingQuery(query) {
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env["OPENAI_API_KEY"]}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: query }],
+      }),
+    };
+
+    try {
+    //   const response = await fetch(process.env["OPENAI_MOCK_API"], options);
+    //   const data = await response.json();
+
+      console.log(process.env);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -17,6 +41,7 @@ export default function InputBox() {
       onSubmit={(e) => submit(e)}
       className="mx-auto flex flex-1 items-center  text-base md:gap-5 lg:gap-6 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem] py-2 bg-white"
     >
+        
       <Input type="text" placeholder="Write Something" ref={queryTextRef} />
       <Button variant="default" size="sm" className="" type="submit">
         Send
